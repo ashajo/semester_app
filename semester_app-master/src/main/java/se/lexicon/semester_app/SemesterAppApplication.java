6503b4614c7b97b6aca127d126e2a3f2f7244693
asha joseph <ashajo183@gmail.com>
2021-06-02 23:49:40 +0200

    Initial commit

---
+++ semester_app-master/src/main/java/se/lexicon/semester_app/SemesterAppApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.semester_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemesterAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SemesterAppApplication.class, args);
+    }
+
+}
---
+++ semester_app-master/src/test/java/se/lexicon/semester_app/SemesterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.semester_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemesterAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
